8e6b11336c06b8ba0f03768d9230c598d70b97ac
urauymanov <urauymanov@yandex.ru>
2021-04-27 15:24:04 +0700

    add data context

---
+++ MvcTemplateNoteManager/Context/NoteManagerContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcTemplateNoteManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcTemplateNoteManager.Context
+{
+    public class NoteManagerContext : DbContext
+    {
+        public NoteManagerContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Models.Task> Tasks { get; set; }
+    }
+}
