4218a90f8b99dc7dfd343f70eee9f54975d2f711
urauymanov <urauymanov@yandex.ru>
2021-04-27 15:06:03 +0700

    Add note model

---
+++ MvcTemplateNoteManager/Models/Note.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MvcTemplateNoteManager.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Models.Task> Tasks { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
