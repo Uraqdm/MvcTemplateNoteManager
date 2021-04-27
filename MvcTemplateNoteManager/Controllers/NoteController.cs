820ed92d800cea653235438bc5defd40b4abe140
urauymanov <urauymanov@yandex.ru>
2021-04-28 00:14:00 +0700

    Add view list of notes via template

--- MvcTemplateNoteManager/Controllers/NoteController.cs
+++ MvcTemplateNoteManager/Controllers/NoteController.cs
@@ -1,17 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MvcTemplateNoteManager.Context;
 
 namespace MvcTemplateNoteManager.Controllers
 {
     public class NoteController : Controller
     {
+        private readonly NoteManagerContext _context;
+
+        public NoteController(NoteManagerContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(_context.Notes);
         }
     }
 }
