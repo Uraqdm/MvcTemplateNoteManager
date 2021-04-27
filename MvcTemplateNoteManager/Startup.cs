00f33887bd52a2d3a5ab82779a9a70ebfe166dc0
urauymanov <urauymanov@yandex.ru>
2021-04-27 15:33:22 +0700

    Add connection string and DI in startup

--- MvcTemplateNoteManager/Startup.cs
+++ MvcTemplateNoteManager/Startup.cs
@@ -1,13 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MvcTemplateNoteManager.Context;
 
 namespace MvcTemplateNoteManager
 {
@@ -24,6 +21,7 @@ namespace MvcTemplateNoteManager
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<NoteManagerContext>(opts => opts.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
