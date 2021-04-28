b16f99c02c9899984726856fbe03a64d6c38add8
urauymanov <urauymanov@yandex.ru>
2021-04-28 16:36:32 +0700

    Remove privacy policy and adding about pages. Also changing method name in home controller.

--- MvcTemplateNoteManager/Controllers/HomeController.cs
+++ MvcTemplateNoteManager/Controllers/HomeController.cs
@@ -19,7 +19,7 @@ namespace MvcTemplateNoteManager.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
